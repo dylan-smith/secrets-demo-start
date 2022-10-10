bac25d69eb96337a5f19bb77752f77397ab46813
lmnleaf <lmnleaf@users.noreply.github.com>
2022-10-10 19:35:26 +0000

    adding a token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 let auth_key = 'fb50ec73d7366c487248b80e1f97a8b2ec550b82'
 var token = "8256ba5a40014ed318f8f22cc15227b579e341ef"
 var token2 = '9278b9f396e023a0680f6100ce9371b2e7619827'
+var token3 = '3338b9f396e023a0680f6100ce9371b2e7619827'
 
 var tok = "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"
 
